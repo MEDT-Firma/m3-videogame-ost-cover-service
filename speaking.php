e0375e5ef3ec035043beabcc97425a881cd5f31e
kopsi1209 <71011485+kopsi1209@users.noreply.github.com>
2021-01-25 14:14:55 +0100

    added speaking url

--- speaking.php
+++ speaking.php
@@ -1,1 +1,40 @@
 <?php
+include_once ("seeder.php");
+
+
+
+// wenn im browser aufgerufen wird eine json zurückgeliefert
+
+header("Content-Type: application/json");
+
+$data = Seeder::getDemoData();
+
+if (!isset($_GET["ost_id"]))
+    echo json_encode([
+        "success" => true,
+        "data" => $data
+    ]);
+
+else {
+    $parsed = urlencode($_GET["ost_id"]);
+    if (is_numeric($parsed) && is_int(intval($parsed))) {
+        $specific_ost = null;
+        for ($i = 0; $i < sizeof($data); $i++)
+            if ($data[$i]->uid == intval($parsed))
+                $specific_ost = $data[$i];
+        if ($specific_ost != null)
+            echo json_encode([
+                "success" => true,
+                "data" => $specific_ost
+            ]);
+        else
+            echo json_encode([
+                "success" => false,
+                "message" => "no ost with id '$parsed' was found"
+            ]);
+    } else
+        echo json_encode([
+            "success" => false,
+            "message" => "specified ost id '$parsed' is not a valid id"
+        ]);
+}
